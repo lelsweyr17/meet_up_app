475c572e823bd58a6b1af4a19cb5f02391d2906a
arinaburyy <buryy.arina@gmail.com>
2021-12-26 16:39:20 +0300

    Add routes class

---
+++ lib/internal/routes.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:meet_up_app/data/service/auth_service.dart';
+import 'package:meet_up_app/presentation/pages/home_page.dart';
+import 'package:meet_up_app/presentation/pages/login_page.dart';
+import 'package:meet_up_app/presentation/pages/sign_up_page.dart';
+import 'package:meet_up_app/presentation/pages/start_page.dart';
+import 'package:meet_up_app/utils/log.dart';
+
+const _tag = "routes";
+
+class Routes {
+  Routes._create() {
+    Log.message(_tag, "_init");
+  }
+
+  static final Routes _instance = Routes._create();
+
+  static Routes get instance => _instance;
+
+  String get initialRoute => (AuthService.instance.isUserAnonymous == true)
+      ? Routes.instance.homePage
+      : Routes.instance.startPage;
+
+  Map<String, Widget Function(BuildContext)> get routes => {
+        startPage: (context) => const StartPage(),
+        homePage: (context) => const HomePage(),
+        loginPage: (context) => const LoginPage(),
+        signUpPage: (context) => const SignUpPage(),
+      };
+
+  String get startPage => "/start";
+
+  String get homePage => "/home";
+
+  String get loginPage => "/login";
+
+  String get signUpPage => "/signUp";
+}
