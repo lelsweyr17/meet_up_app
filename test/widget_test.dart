ed568e9fb9f47039eb1e4d24292b0cb0ae3dc71a
arinaburyy <buryy.arina@gmail.com>
2021-12-26 16:41:26 +0300

    Update widget_test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:meet_up_app/presentation/app.dart';
+import 'package:meet_up_app/internal/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
