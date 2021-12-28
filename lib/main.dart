31bda9818cb96467a4b142a33ff144ebd42b6cc2
arinaburyy <buryy.arina@gmail.com>
2021-12-28 23:55:13 +0300

    Add PreferencesService initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:meet_up_app/internal/app.dart';
+import 'package:meet_up_app/services/preferences_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await PreferencesService.init();
   runApp(const App());
 }
