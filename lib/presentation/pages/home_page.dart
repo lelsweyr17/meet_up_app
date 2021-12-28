83090b834faec83e5c15f7b70f905a21bfbc0a11
arinaburyy <buryy.arina@gmail.com>
2021-12-28 23:55:00 +0300

    add AppBar

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -29,6 +29,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       drawer: const HomeDrawer(),
       body: Center(
         child: Column(
