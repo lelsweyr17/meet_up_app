ffee440e699e46ed470a150dc90f6ff0c23a8853
arinaburyy <buryy.arina@gmail.com>
2021-12-29 14:39:25 +0300

    Update condition for initial route

--- lib/internal/routes.dart
+++ lib/internal/routes.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:meet_up_app/data/service/auth_service.dart';
 import 'package:meet_up_app/presentation/pages/home_page.dart';
 import 'package:meet_up_app/presentation/pages/login_page.dart';
 import 'package:meet_up_app/presentation/pages/sign_up_page.dart';
@@ -18,10 +17,9 @@ class Routes {
 
   static Routes get instance => _instance;
 
-  String get initialRoute => (AuthService.instance.isUserAnonymous == true ||
-          !PreferencesService.instance.isShowStartPage)
-      ? Routes.instance.homePage
-      : Routes.instance.startPage;
+  String get initialRoute => (PreferencesService.instance.isShowStartPage)
+      ? Routes.instance.startPage
+      : Routes.instance.homePage;
 
   Map<String, Widget Function(BuildContext)> get routes => {
         startPage: (context) => const StartPage(),
