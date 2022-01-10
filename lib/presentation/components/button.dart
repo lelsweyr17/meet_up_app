3dd48c0c6f8a1c1ccd5cc3456d80faae6d60ef57
arinaburyy <buryy.arina@gmail.com>
2022-01-10 17:52:55 +0300

    New start page. Button update

--- lib/presentation/components/button.dart
+++ lib/presentation/components/button.dart
@@ -4,16 +4,39 @@ class Button extends StatelessWidget {
   const Button({
     Key? key,
     required this.text,
+    this.textColor = Colors.white,
+    this.buttonColor = Colors.transparent,
     this.onPressed,
   }) : super(key: key);
 
   final String text;
+  final Color textColor;
+  final Color buttonColor;
   final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle outlineButtonStyle = OutlinedButton.styleFrom(
+      primary: textColor,
+      backgroundColor: buttonColor,
+      minimumSize: const Size(88, 46),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
+      side: BorderSide(
+        color: textColor == Colors.white ? textColor : Colors.transparent,
+        width: 1,
+      ),
+      textStyle: const TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+
     return OutlinedButton(
       onPressed: onPressed,
+      style: outlineButtonStyle,
       child: Text(text),
     );
   }
--- lib/presentation/pages/start_page.dart
+++ lib/presentation/pages/start_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:meet_up_app/domain/reusable/auth_reusable.dart';
 import 'package:meet_up_app/l10n/app_localizations_export.dart';
-import 'package:meet_up_app/presentation/components/logInAndSignUpButtons.dart';
+import 'package:meet_up_app/presentation/components/button.dart';
 
 const _tag = "start_page";
 
@@ -12,20 +13,62 @@ class StartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final _localizations = AppLocalizations.of(context)!;
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const LogInAndSignUpButtons(),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: InkWell(
-                child: Text(_localizations.skip),
-                onTap: () => onSkipPressed(context),
-              ),
-            )
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        gradient: LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          colors: [
+            Color(0xFFFF9F1C),
+            Color(0xFFEE6071),
           ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "See ya",
+                  style: GoogleFonts.nunito(
+                    fontSize: 48,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Button(
+                          text: AppLocalizations.of(context)!.logIn,
+                          onPressed: () => onNavigateLogInPressed(context),
+                        ),
+                      ),
+                      const SizedBox(width: 12),
+                      Expanded(
+                        child: Button(
+                          textColor: Colors.deepOrangeAccent,
+                          buttonColor: Colors.white,
+                          text: AppLocalizations.of(context)!.signUp,
+                          onPressed: () => onNavigateToSignUpPressed(context),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
