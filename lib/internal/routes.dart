43ba14af7b63baabdbed62b877c009a6c0e7f167
arinaburyy <buryy.arina@gmail.com>
2022-01-10 16:35:21 +0300

    Add routes

--- lib/internal/routes.dart
+++ lib/internal/routes.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:meet_up_app/presentation/pages/home_page.dart';
 import 'package:meet_up_app/presentation/pages/login_page.dart';
+import 'package:meet_up_app/presentation/pages/my_meetings_page.dart';
+import 'package:meet_up_app/presentation/pages/profile_page.dart';
+import 'package:meet_up_app/presentation/pages/settings_page.dart';
 import 'package:meet_up_app/presentation/pages/sign_up_page.dart';
 import 'package:meet_up_app/presentation/pages/start_page.dart';
 import 'package:meet_up_app/services/preferences_service.dart';
@@ -26,6 +29,9 @@ class Routes {
         homePage: (context) => const HomePage(),
         loginPage: (context) => const LoginPage(),
         signUpPage: (context) => const SignUpPage(),
+        settings: (context) => const SettingsPage(),
+        profile: (context) => const ProfilePage(),
+        myMeetings: (context) => const MyMeetingsPage(),
       };
 
   String get startPage => "/start";
@@ -35,4 +41,19 @@ class Routes {
   String get loginPage => "/login";
 
   String get signUpPage => "/signUp";
+
+  String get settings => "/settings";
+
+  String get profile => "/profile";
+
+  String get myMeetings => "/myMeetings";
+
+  void navigateTo(
+    String routeName,
+    BuildContext context,
+  ) =>
+      Navigator.pushNamed(
+        context,
+        routeName,
+      );
 }
