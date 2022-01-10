285785b6137a1d266b378bad271792ae46e9b0a7
arinaburyy <buryy.arina@gmail.com>
2022-01-10 16:35:12 +0300

    Create empty pages from drawer

---
+++ lib/presentation/pages/my_meetings_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyMeetingsPage extends StatelessWidget {
+  const MyMeetingsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("My meetings")),
+    );
+  }
+}
