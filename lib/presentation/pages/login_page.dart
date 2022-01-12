a4d9f0d41d29754a6436d4b34eabf8173769faab
arinaburyy <buryy.arina@gmail.com>
2022-01-12 17:58:49 +0300

    Add keyboard dismiss on tap to login page

--- lib/presentation/pages/login_page.dart
+++ lib/presentation/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:meet_up_app/domain/bloc/auth/auth_bloc.dart';
 import 'package:meet_up_app/domain/bloc/auth/auth_state.dart';
 import 'package:meet_up_app/domain/reusable/auth_reusable.dart';
@@ -39,60 +40,62 @@ class _LoginPageState extends State<LoginPage> {
     return BlocBuilder(
       bloc: _authBloc,
       builder: (BuildContext context, state) {
-        return Scaffold(
-          appBar: AppBars.instance.transparentAppBarWithBlackStatusBar(),
-          body: Center(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 48),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      clipBehavior: Clip.antiAlias,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(70),
+        return KeyboardDismissOnTap(
+          child: Scaffold(
+            appBar: AppBars.instance.transparentAppBarWithBlackStatusBar(),
+            body: Center(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 48),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(70),
+                        ),
+                        child: (PeopleImages.person1..size = 140).build(),
                       ),
-                      child: (PeopleImages.person1..size = 140).build(),
-                    ),
-                    const SizedBox(height: 50),
-                    LoginTextField(
-                      hintText: _localizations.enterYourEmail,
-                      obscure: false,
-                      onChanged: (value) => _authBloc.emailSink.add(value),
-                    ),
-                    const SizedBox(height: 16),
-                    LoginTextField(
-                      hintText: _localizations.enterYourPassword,
-                      obscure: true,
-                      onChanged: (value) => _authBloc.passwordSink.add(value),
-                    ),
-                    const SizedBox(height: 20),
-                    if (state is LoadingAuthentication)
-                      const SizedBox(
-                        height: 48,
-                        child: LoadingIndicator(),
-                      )
-                    else
-                      LinearGradientButton(
-                        text: _localizations.logIn,
-                        onPressed: () => onLogInPressed(
-                          authBloc: _authBloc,
-                          context: context,
+                      const SizedBox(height: 50),
+                      LoginTextField(
+                        hintText: _localizations.enterYourEmail,
+                        obscure: false,
+                        onChanged: (value) => _authBloc.emailSink.add(value),
+                      ),
+                      const SizedBox(height: 16),
+                      LoginTextField(
+                        hintText: _localizations.enterYourPassword,
+                        obscure: true,
+                        onChanged: (value) => _authBloc.passwordSink.add(value),
+                      ),
+                      const SizedBox(height: 20),
+                      if (state is LoadingAuthentication)
+                        const SizedBox(
+                          height: 48,
+                          child: LoadingIndicator(),
+                        )
+                      else
+                        LinearGradientButton(
+                          text: _localizations.logIn,
+                          onPressed: () => onLogInPressed(
+                            authBloc: _authBloc,
+                            context: context,
+                          ),
+                        ),
+                      const SizedBox(height: 20),
+                      TextButton(
+                        onPressed: () => onSkipPressed(context),
+                        child: Text(
+                          _localizations.skip,
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black.withOpacity(0.3),
+                          ),
                         ),
                       ),
-                    const SizedBox(height: 20),
-                    TextButton(
-                      onPressed: () => onSkipPressed(context),
-                      child: Text(
-                        _localizations.skip,
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.black.withOpacity(0.3),
-                        ),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
