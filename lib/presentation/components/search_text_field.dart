3948f3c25a4d3fa9847d03ccb5c767d9b1484249
arinaburyy <buryy.arina@gmail.com>
2022-01-12 22:07:38 +0300

    Create SearchTextField

---
+++ lib/presentation/components/search_text_field.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:meet_up_app/utils/app_icons.dart';
+
+class SearchTextField extends StatelessWidget {
+  const SearchTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Container(
+        height: 50,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Theme.of(context).scaffoldBackgroundColor,
+        ),
+        child: TextFormField(
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 12,
+              vertical: 14,
+            ),
+            border: InputBorder.none,
+            hintText: "Search",
+            hintStyle: const TextStyle(color: Color(0xFFB3B3B3)),
+            suffixIcon: Padding(
+              padding: const EdgeInsetsDirectional.only(end: 6.0),
+              child: IconButton(
+                icon:
+                    (AppIcons.search..color = const Color(0xFFB3B3B3)).build(),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
