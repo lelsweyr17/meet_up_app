9a9d778157b010a9ce3067c842b3b5a5f7d9318b
arinaburyy <buryy.arina@gmail.com>
2022-01-12 16:10:51 +0300

    Replace button by linear gradient button

--- lib/presentation/components/logInAndSignUpButtons.dart
+++ lib/presentation/components/logInAndSignUpButtons.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meet_up_app/domain/reusable/auth_reusable.dart';
 import 'package:meet_up_app/l10n/app_localizations_export.dart';
-import 'package:meet_up_app/presentation/components/button.dart';
+import 'package:meet_up_app/presentation/components/buttons/lineat_gradient_button.dart';
 
 class LogInAndSignUpButtons extends StatelessWidget {
   const LogInAndSignUpButtons({Key? key}) : super(key: key);
@@ -11,11 +11,12 @@ class LogInAndSignUpButtons extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Button(
+        LinearGradientButton(
           text: AppLocalizations.of(context)!.logIn,
           onPressed: () => onNavigateLogInPressed(context),
         ),
-        Button(
+        const SizedBox(height: 10),
+        LinearGradientButton(
           text: AppLocalizations.of(context)!.signUp,
           onPressed: () => onNavigateToSignUpPressed(context),
         ),
