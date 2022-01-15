6dd42123601b7120e731eddf2f6a339be5dc1916
arinaburyy <buryy.arina@gmail.com>
2022-01-15 16:28:56 +0300

    Add tests for start page (widget_test)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,22 +1,52 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:meet_up_app/internal/app.dart';
+import 'package:meet_up_app/presentation/components/buttons/button.dart';
+import 'package:meet_up_app/presentation/pages/start_page.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+Widget makeTestableWidget({
+  required Widget child,
+  Locale locale = const Locale("en"),
+}) {
+  return MaterialApp(
+    localizationsDelegates: AppLocalizations.localizationsDelegates,
+    supportedLocales: AppLocalizations.supportedLocales,
+    locale: locale,
+    home: child,
+  );
+}
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+void main() async {
+  group("Start page tests", () {
+    testWidgets('Widgets exist in RU', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        makeTestableWidget(
+          child: const StartPage(),
+          locale: Locale("ru"),
+        ),
+      );
+      await tester.pumpAndSettle();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      expect(find.text("See ya"), findsOneWidget);
+      expect(find.widgetWithText(Button, "Вход"), findsOneWidget);
+      expect(find.widgetWithText(Button, "Регистрация"), findsOneWidget);
+    });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    testWidgets('Widgets exist in EN', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        makeTestableWidget(child: const StartPage()),
+      );
+      await tester.pumpAndSettle();
+
+      expect(find.text("See ya"), findsOneWidget);
+      expect(find.widgetWithText(Button, "Log In"), findsOneWidget);
+      expect(find.widgetWithText(Button, "Sign Up"), findsOneWidget);
+    });
   });
 }
