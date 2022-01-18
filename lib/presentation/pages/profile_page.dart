82e7e825407e5832069ab80f26eacd8e67fa27b2
arinaburyy <buryy.arina@gmail.com>
2022-01-18 22:15:14 +0300

    Big update of ProfilePage

--- lib/presentation/pages/profile_page.dart
+++ lib/presentation/pages/profile_page.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:meet_up_app/data/service/auth_service.dart';
+import 'package:meet_up_app/domain/bloc/auth/auth_bloc.dart';
+import 'package:meet_up_app/domain/reusable/auth_reusable.dart';
 import 'package:meet_up_app/l10n/app_localizations_export.dart';
+import 'package:meet_up_app/presentation/components/app_bars.dart';
+import 'package:meet_up_app/presentation/components/avatar.dart';
+import 'package:meet_up_app/presentation/components/logIn_and_sign_up_buttons_page.dart';
+import 'package:meet_up_app/presentation/components/rating.dart';
+import 'package:meet_up_app/utils/app_icons.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -7,8 +15,232 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.profile),
+      body: AuthService.instance.isUserAnonymousOrNull
+          ? const LogInAndSignUpButtonsPage()
+          : const _Body(),
+    );
+  }
+}
+
+class _AvatarAndRating extends StatelessWidget {
+  const _AvatarAndRating({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Transform.translate(
+          offset: const Offset(0, 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              Avatar(size: 120),
+              Rating(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomScrollView(
+      slivers: [
+        _AppBarWithAvatarAndRating(),
+        _ProfileDescription(),
+        _ProfileSettings(),
+      ],
+    );
+  }
+}
+
+class _ProfileDescription extends StatelessWidget {
+  const _ProfileDescription({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 20,
+        ),
+        child: Column(
+          children: const [
+            _TitleAndSubtitle(
+              title: "Арина",
+              subtitle: "Имя",
+            ),
+            Divider(height: 0),
+            _TitleAndSubtitle(
+              title: "Мобильный разработчик",
+              subtitle: "О себе",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _AppBarWithAvatarAndRating extends StatelessWidget {
+  const _AppBarWithAvatarAndRating({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBars.instance.sliverAppBarWithGradient(
+      size: 120,
+      title: AppLocalizations.of(context)!.profile,
+      child: const _AvatarAndRating(),
+    );
+  }
+}
+
+class _TitleAndSubtitle extends StatelessWidget {
+  const _TitleAndSubtitle({
+    Key? key,
+    required this.title,
+    this.subtitle = "",
+  }) : super(key: key);
+
+  final String title;
+  final String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      dense: true,
+      contentPadding: EdgeInsets.zero,
+      title: Text(
+        title,
+        style: const TextStyle(fontSize: 18),
+      ),
+      subtitle: Text(
+        subtitle,
+        style: const TextStyle(fontSize: 14),
+      ),
+    );
+  }
+}
+
+class RoundedBorderContainerWithChild extends StatelessWidget {
+  const RoundedBorderContainerWithChild({
+    Key? key,
+    required this.child,
+    this.onPressed,
+  }) : super(key: key);
+
+  final Widget child;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.white,
+            ),
+            child: child,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LeadingAndTitle extends StatelessWidget {
+  const LeadingAndTitle({
+    Key? key,
+    required this.leading,
+    required this.title,
+    this.titleColor = Colors.black,
+  }) : super(key: key);
+
+  final Widget leading;
+  final String title;
+  final Color titleColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      minLeadingWidth: 0,
+      leading: leading,
+      title: Text(
+        title,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          color: titleColor,
+          fontSize: 16,
+          fontWeight: FontWeight.normal,
+          letterSpacing: -0.33,
+        ),
+      ),
+    );
+  }
+}
+
+class _ProfileSettings extends StatelessWidget {
+  const _ProfileSettings({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final authBloc = AuthBloc();
+
+    return SliverFillRemaining(
+      hasScrollBody: false,
+      child: Container(
+        color: const Color(0xfff5f5f5),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            RoundedBorderContainerWithChild(
+              onPressed: () {},
+              child: LeadingAndTitle(
+                title: "Change profile photo",
+                titleColor: Colors.orange,
+                leading: (AppIcons.addPhoto..color = Colors.orange).build(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            RoundedBorderContainerWithChild(
+              onPressed: () {},
+              child: Column(
+                children: [
+                  LeadingAndTitle(
+                    title: "Meetings",
+                    leading: (AppIcons.calendar).build(),
+                  ),
+                  const Divider(height: 0),
+                  LeadingAndTitle(
+                    title: "Settings",
+                    leading: (AppIcons.settings).build(),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            RoundedBorderContainerWithChild(
+              onPressed: () =>
+                  onLogOutPressed(context: context, authBloc: authBloc),
+              child: LeadingAndTitle(
+                title: "Log out",
+                leading: AppIcons.logOut.build(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
