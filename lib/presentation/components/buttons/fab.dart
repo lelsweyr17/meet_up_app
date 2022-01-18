fd7e923d62e3d95d1f885e17404d43796decc53d
arinaburyy <buryy.arina@gmail.com>
2022-01-18 22:11:31 +0300

    Update FAB

--- lib/presentation/components/buttons/fab.dart
+++ lib/presentation/components/buttons/fab.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:meet_up_app/utils/gradients.dart';
 import 'package:meet_up_app/utils/shadows.dart';
 
 class Fab extends StatelessWidget {
@@ -12,21 +11,27 @@ class Fab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 60,
-      width: 60,
       decoration: BoxDecoration(
-        gradient: Gradients.instance.yellowToRed(),
-        borderRadius: BorderRadius.circular(30),
-        boxShadow: [
-          Shadows.instance.black8(),
-        ],
+        borderRadius: BorderRadius.circular(24),
+        boxShadow: [Shadows.instance.black8()],
       ),
-      child: Center(
-        child: IconButton(
-          onPressed: onTap,
-          splashRadius: 30,
-          icon: const Icon(Icons.add_rounded, size: 30),
-          color: Colors.white,
+      child: FloatingActionButton.extended(
+        onPressed: () {},
+        elevation: 0,
+        highlightElevation: 0,
+        clipBehavior: Clip.antiAlias,
+        label: const Text(
+          "Create meeting",
+          style: TextStyle(
+            fontSize: 16,
+            letterSpacing: -0.33,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        icon: const Icon(
+          Icons.add_rounded,
+          color: Colors.orange,
         ),
       ),
     );
