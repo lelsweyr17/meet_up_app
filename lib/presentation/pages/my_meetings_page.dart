0e6652062af1a82ca164170a1d8e7290f92cb92b
arinaburyy <buryy.arina@gmail.com>
2022-01-18 22:16:39 +0300

    Update MyMeetingPage with transparent app bar

--- lib/presentation/pages/my_meetings_page.dart
+++ lib/presentation/pages/my_meetings_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meet_up_app/l10n/app_localizations_export.dart';
+import 'package:meet_up_app/presentation/components/app_bars.dart';
 
 class MyMeetingsPage extends StatelessWidget {
   const MyMeetingsPage({Key? key}) : super(key: key);
@@ -7,7 +8,9 @@ class MyMeetingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(AppLocalizations.of(context)!.myMeetings)),
+      appBar: AppBars.instance.transparentAppBarWithBlackStatusBar(
+        title: AppLocalizations.of(context)!.myMeetings,
+      ),
     );
   }
 }
