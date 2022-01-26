dddfc7f5d0da2026616ce2cc2c032b01890b9b24
arinaburyy <buryy.arina@gmail.com>
2022-01-26 14:46:56 +0300

    Update serializers

--- lib/data/model/serializers.dart
+++ lib/data/model/serializers.dart
@@ -1,3 +1,4 @@
+import 'package:built_value/iso_8601_date_time_serializer.dart';
 import 'package:built_value/serializer.dart';
 import 'package:built_value/standard_json_plugin.dart';
 import 'package:meet_up_app/data/model/client/user.dart';
@@ -5,23 +6,19 @@ import 'package:meet_up_app/utils/log.dart';
 
 part 'serializers.g.dart';
 
+const _tag = "serializers";
+
 @SerializersFor([
   User,
 ])
 final Serializers serializers = (_$serializers.toBuilder()
-      ..addPlugin(
-        StandardJsonPlugin(),
-      ))
+      ..addPlugin(StandardJsonPlugin())
+      ..add(Iso8601DateTimeSerializer()))
     .build();
-
-Object? serialize<T>(T value) => serializers.serializeWith(
-      serializers.serializerForType(T) as Serializer<T>,
-      value,
-    );
 
 T? deserialize<T>(dynamic value) {
   try {
-    final serializer = serializers.serializerForType(T) as Serializer<T>;
+    final serializer = serializers.serializerForType(T) as Serializer<T>?;
 
     if (serializer != null) {
       return serializers.deserializeWith<T>(
@@ -29,9 +26,24 @@ T? deserialize<T>(dynamic value) {
         value,
       );
     }
-    return null;
   } on Exception catch (e) {
-    Log.message("serializers", "${value["\$"]}: ${value["id"]}\n$e");
-    return null;
+    Log.message(_tag, "${value["\$"]}: ${value["id"]}\n$e");
   }
+}
+
+Object? serialize<T>(T value) {
+  print("serializers.serialize(value) ${serializers.serialize(value)}");
+  return serializers.serialize(value);
+}
+
+List<T> deserializeIterable<T>(Iterable<Map<dynamic, dynamic>> value) => value
+    .map((value) => deserialize<T>(value))
+    .whereType<T>()
+    .toList(growable: false);
+
+List<Object> serializeIterable<T>(Iterable<T> value) {
+  return value
+      .map((value) => serialize<T>(value))
+      .whereType<Object>()
+      .toList(growable: false);
 }
